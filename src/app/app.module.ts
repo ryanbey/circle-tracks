99d9cb653bc280720da81680d3bc10d14a86b658
ryanbey <beycommaryan@gmail.com>
2022-06-28 08:25:37 -0600

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TrackListComponent } from './track-list/track-list.component';
+import { TrackItemComponent } from './track-item/track-item.component';
+import { TrackDetailComponent } from './track-detail/track-detail.component';
+import { CategoryCardComponent } from './category-card/category-card.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TrackListComponent,
+    TrackItemComponent,
+    TrackDetailComponent,
+    CategoryCardComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/track-detail/track-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-track-detail',
+  templateUrl: './track-detail.component.html',
+  styleUrls: ['./track-detail.component.scss']
+})
+export class TrackDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
