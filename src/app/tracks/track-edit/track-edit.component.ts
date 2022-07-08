b401579a651b82884259bd6a05eade5333047173
Ryan Bey <bey15002@byui.edu>
2022-07-07 20:24:32 -0600

    implemented track edit typescript

--- src/app/tracks/track-edit/track-edit.component.ts
+++ src/app/tracks/track-edit/track-edit.component.ts
@@ -1,15 +1,78 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Track } from '../track.model';
+import { TrackService } from '../tracks.service';
 
 @Component({
   selector: 'app-track-edit',
   templateUrl: './track-edit.component.html',
-  styleUrls: ['./track-edit.component.scss']
+  styleUrls: ['./track-edit.component.scss'],
 })
 export class TrackEditComponent implements OnInit {
+  originalTrack;
+  track: Track;
+  editMode = false;
+  id: string;
 
-  constructor() { }
+  constructor(
+    private trackService: TrackService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      const id = params['id'];
+      if (id === undefined || id === null) {
+        this.editMode = false;
+        return;
+      }
+
+      this.originalTrack = this.trackService.getTrack(id);
+
+      if (this.originalTrack === undefined || this.originalTrack === null) {
+        return;
+      }
+      this.editMode = true;
+      this.track = JSON.parse(JSON.stringify(this.originalTrack));
+    });
   }
 
+  onSubmit(form: NgForm) {
+    const value = form.value;
+    const newTrack = new Track(
+      '',
+      value.id,
+      value.name,
+      value.built,
+      value.length,
+      value.surface,
+      value.turns,
+      value.banking,
+      value.capacity,
+      "assets/images/track-maps/" + value.mapUrl,
+      "assets/images/track-images/" + value.imageUrl
+    );
+    if (this.editMode) {
+      this.trackService.updateTrack(this.originalTrack, newTrack);
+    } else {
+      this.trackService.addTrack(newTrack);
+    }
+    this.onCancel();
+  }
+
+  onCancel() {
+    this.router.navigate(['/tracks']);
+  }
+
+  isInvalidTrack(newTrack: Track) {
+    if (!newTrack) {
+      return true;
+    }
+    if (this.track && newTrack.id === this.track.id) {
+      return true;
+    }
+    return false;
+  }
 }
