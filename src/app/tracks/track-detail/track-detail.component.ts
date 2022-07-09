10c4d220d3321c4d978bce5c6b0b4c102edb4d7d
Ryan Bey <bey15002@byui.edu>
2022-07-09 14:50:18 -0600

    track detail stlyed and working

--- src/app/tracks/track-detail/track-detail.component.ts
+++ src/app/tracks/track-detail/track-detail.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Track } from '../track.model';
+import { TrackService } from '../tracks.service';
 
 @Component({
-  selector: 'app-track-detail',
+  selector: 'cms-track-detail',
   templateUrl: './track-detail.component.html',
-  styleUrls: ['./track-detail.component.scss']
+  styleUrls: ['./track-detail.component.scss'],
 })
 export class TrackDetailComponent implements OnInit {
+  track: Track;
+  id: string;
 
-  constructor() { }
+  constructor(
+    private trackService: TrackService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    // Subscribe to active route, get id from active route, get track with that id
+    this.route.params.subscribe((params: Params) => {
+      this.id = params['id'];
+      this.trackService.getTrack(this.id).subscribe((trackData) => {
+        this.track = trackData.track;
+      });
+    });
   }
 
+  onDelete() {
+    this.trackService.deleteTrack(this.track);
+    this.router.navigateByUrl('/tracks');
+  }
 }
